5393f43b0b7543195409cac81d6f1b9ed1338ef1
Deva658 <devaaditya271@gmail.com>
2025-05-05 16:04:41 +0700

    void class siswa

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -12,3 +12,25 @@ public:
     void setNama(string pNama);
     friend class siswa;
 };
+
+class siswa
+{
+private:
+    int id;\
+
+public:
+    void setId(int pId);
+    void displayALL(orang &a);
+};
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+void siswa::displayALL(orang &a)
+{
+    cout << id << endl << a.nama;
+}
